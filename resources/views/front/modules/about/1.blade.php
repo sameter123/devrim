e2973479a77e7e7465e861b964e3c21cdc039293
ttugcecalik <tugce.calik0412@gmail.com>
2021-12-10 18:15:23 +0300

    slider-hakkımızda
    slider ve hakkımızda örneği

---
+++ resources/views/front/modules/about/1.blade.php
@@ -0,0 +1,18 @@
+<section class="about_1 pad-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-12 about-img" data-aos="fade-right" data-aos-duration="1000">
+                <img class="img_1" src="https://images.pexels.com/photos/10135334/pexels-photo-10135334.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                <img class="img_2" src="https://images.pexels.com/photos/10322857/pexels-photo-10322857.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+            </div>
+            <div class="col-lg-6 col-md-6 col-12 about-text" data-aos="fade-left" data-aos-duration="1000">
+                <h6 class="animate__animated animate__bounce">About Us</h6>
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi veniam ipsa iusto error laudantium sint omnis amet vero ex, accusantium molestiae accusamus non ea corrupti assumenda. Totam, labore. Corrupti, debitis.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque quaerat harum beatae, maxime sapiente animi tempora adipisci fugit, doloribus id, aliquid earum fuga repellendus doloremque ipsum commodi quas perspiciatis aspernatur.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto cumque in eius, hic voluptas inventore quaerat vel deserunt aliquam labore? Laborum ad deleniti adipisci itaque ipsam eaque soluta, rem consectetur.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/front/modules/slider/6.blade.php
@@ -0,0 +1,106 @@
+<section class="slider-pages">
+
+  <article class="js-scrolling__page js-scrolling__page-1 js-scrolling--active">
+    <div class="slider-page slider-page--left">
+      <div class="slider-page--skew">
+        <div class="slider-page__content">
+        </div>
+        <!-- /.slider-page__content -->
+      </div>
+      <!-- /.slider-page--skew -->
+    </div>
+    <!-- /.slider-page slider-page--left -->
+
+    <div class="slider-page slider-page--right">
+      <div class="slider-page--skew">
+        <div class="slider-page__content">
+          <h1 class="slider-page__title slider-page__title--big">
+            Gray & White - Skewed Slider with Scrolling
+          </h1>
+          <!-- /.slider-page__title slider-page__title--big -->
+          <h2 class="slider-page__title">
+            Start of your story
+          </h2>
+          <!-- /.slider-page__title -->
+          <p class="slider-page__description">
+            Please scroll down or press the down arrow on your keyboard
+          </p>
+          <!-- /.slider-page__description -->
+        </div>
+        <!-- /.slider-page__content -->
+      </div>
+      <!-- /.slider-page--skew -->
+    </div>
+    <!-- /.slider-page slider-page--right -->
+  </article>
+  <!-- /.js-scrolling__page js-scrolling__page-1 js-scrolling--active -->
+
+
+  <article class="js-scrolling__page js-scrolling__page-2">
+    <div class="slider-page slider-page--left">
+      <div class="slider-page--skew">
+        <div class="slider-page__content">
+          <h1 class="slider-page__title slider-page__title--big">
+            Now you are here
+          </h1>
+          <!-- /.slider-page__title -->
+          <p class="slider-page__description">
+            Please continue scrolling
+          </p>
+          <!-- /.slider-page__description -->
+        </div>
+        <!-- /.slider-page__content -->
+      </div>
+      <!-- /.slider-page--skew -->
+    </div>
+    <!-- /.slider-page slider-page--left -->
+
+    <div class="slider-page slider-page--right">
+      <div class="slider-page--skew">
+        <div class="slider-page__content">
+        </div>
+        <!-- /.slider-page__content -->
+      </div>
+      <!-- /.slider-page--skew -->
+    </div>
+    <!-- /.slider-page slider-page--right -->
+  </article>
+  <!-- /.js-scrolling__page js-scrolling__page-2 -->
+
+
+  <article class="js-scrolling__page js-scrolling__page-3">
+    <div class="slider-page slider-page--left">
+      <div class="slider-page--skew">
+        <div class="slider-page__content">
+        </div>
+        <!-- /.slider-page__content -->
+      </div>
+      <!-- /.slider-page--skew -->
+    </div>
+    <!-- /.slider-page slider-page--left -->
+
+    <div class="slider-page slider-page--right">
+      <div class="slider-page--skew">
+        <div class="slider-page__content">
+          <h1 class="slider-page__title slider-page__title--big">
+            Final is just the beginning
+          </h1>
+          <!-- /.slider-page__title -->
+          <p class="slider-page__description">
+            Feel free to follow me on <a class="slider-page__link" href="https://twitter.com/WispProxy"
+                                         target="_blank">Twitter</a>
+            and check of <a class="slider-page__link" href="https://codepen.io/WispProxy/pens/public"
+                            target="_blank">my other works</a>
+          </p>
+          <!-- /.slider-page__description -->
+        </div>
+        <!-- /.slider-page__content -->
+      </div>
+      <!-- /.slider-page--skew -->
+    </div>
+    <!-- /.slider-page slider-page--right -->
+  </article>
+  <!-- /.js-scrolling__page js-scrolling__page-3 -->
+
+</section>
+<!-- /.slider-pages -->
